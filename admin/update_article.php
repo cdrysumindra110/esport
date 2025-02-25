5c4d095d6c5374bdfe3df4647d809ab058698e32
Sumindra Chaudhary <cdrysumindra110@gmail.com>
2025-02-25 18:56:31 +0545

    Update update_article.php

--- admin/update_article.php
+++ admin/update_article.php
@@ -96,9 +96,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
-<div id="preloader" style="background: #1E1E2F url(../img/loader.gif) no-repeat center center; 
-        background-size: 4.5%;height: 100vh;width: 100%;position: fixed;z-index: 999;">
-        </div>
+
 <div class="popup-message" id="popup-message"></div>
       <header class="page-header">
         <nav>
